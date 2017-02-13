10aa2229fab51cc13822f597ff99d6827e6f8a92
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2017-02-13 19:24:24 +0100

    Introduce API endpoint that returns fake data for now.

--- api/src/main.rs
+++ api/src/main.rs
@@ -8,6 +8,13 @@ extern crate iron;
 extern crate log;
 extern crate router;
 extern crate rustc_serialize;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+#[macro_use]
+extern crate serde_json;
+
+mod core;
 
 use docopt::Docopt;
 
@@ -38,6 +45,40 @@ fn handler(req: &mut Request) -> IronResult<Response> {
     Ok(Response::with((status::Ok, *query)))
 }
 
+fn get_control_groups_handler(_: &mut Request) -> IronResult<Response> {
+    let content_type = "application/json".parse::<iron::mime::Mime>().unwrap();
+
+    let fake_response = vec![core::control_group::ControlGroup {
+        type_name: "group#1".to_owned(),
+        name: "Group #1".to_owned(),
+        description: "Group #1 Description".to_owned(),
+        items: vec![core::control_metadata::ControlMetadata {
+            type_name: "type#11".to_owned(),
+            name: "Item #11".to_owned(),
+            description: "Item #11 Description".to_owned(),
+        }, core::control_metadata::ControlMetadata {
+            type_name: "type#12".to_owned(),
+            name: "Item #12".to_owned(),
+            description: "Item #12 Description".to_owned(),
+        }]
+    }, core::control_group::ControlGroup {
+        type_name: "group#2".to_owned(),
+        name: "Group #2".to_owned(),
+        description: "Group #2 Description".to_owned(),
+        items: vec![core::control_metadata::ControlMetadata {
+            type_name: "type#21".to_owned(),
+            name: "Item #21".to_owned(),
+            description: "Item #21 Description".to_owned(),
+        }, core::control_metadata::ControlMetadata {
+            type_name: "type#22".to_owned(),
+            name: "Item #22".to_owned(),
+            description: "Item #22 Description".to_owned(),
+        }]
+    }];
+
+    Ok(Response::with((content_type, status::Ok, serde_json::to_string(&fake_response).unwrap())))
+}
+
 fn main() {
     env_logger::init().unwrap();
 
@@ -48,6 +89,7 @@ fn main() {
     let mut router = Router::new();
     router.get("/", handler, "index");
     router.get("/:query", handler, "query");
+    router.get("/control-groups", get_control_groups_handler, "control-groups");
 
     let ip = args.flag_ip.unwrap_or("0.0.0.0".to_owned());
     let port = args.flag_port.unwrap_or(8009);
