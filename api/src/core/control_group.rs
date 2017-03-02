d716a9c1968ae8ede4f36674d000af5a4b2e6518
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2017-03-02 22:00:20 +0100

    Synchronize serialize and deserialize field name overrides.

--- api/src/core/control_group.rs
+++ api/src/core/control_group.rs
@@ -2,7 +2,7 @@ use core::control_metadata::ControlMetadata;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ControlGroup {
-    #[serde(rename(serialize = "type"))]
+    #[serde(rename(serialize = "type", deserialize = "type"))]
     pub type_name: String,
     pub name: String,
     pub description: String,
--- api/src/core/control_metadata.rs
+++ api/src/core/control_metadata.rs
@@ -1,6 +1,6 @@
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ControlMetadata {
-    #[serde(rename(serialize = "type"))]
+    #[serde(rename(serialize = "type", deserialize = "type"))]
     pub type_name: String,
     pub name: String,
     pub description: String,
