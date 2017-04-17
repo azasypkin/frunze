fe080b68b539f9bfdf777980d59acebef7121312
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2017-04-17 19:48:59 +0200

    Move navigation to a "master" view.

--- ng-app/src/modules/app/components/bootstrap/bootstrap.component.ts
+++ ng-app/src/modules/app/components/bootstrap/bootstrap.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'frunze-root',
-  templateUrl: 'bootstrap.component.html'
+  templateUrl: 'bootstrap.component.html',
+  styleUrls: ['bootstrap.component.css']
 })
 export class BootstrapComponent {
 }
