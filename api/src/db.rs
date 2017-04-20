ff4035d163fbcc0f147c834c8babb5b8e1bcd3eb
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2017-04-20 17:33:47 +0200

    [DB] Extend the list of available capabilities.

--- api/src/db.rs
+++ api/src/db.rs
@@ -128,16 +128,31 @@ impl DB {
         let project_capability_groups = vec![ProjectCapabilityGroup {
             type_name: "connectivity".to_string(),
             name: "Connectivity".to_string(),
-            hint: "Items in this group allows your device to be connected to somewhere (Internet, \
-                other device etc.).".to_string(),
+            hint: "Everything that allows devices to be connected.".to_string(),
             capabilities: vec![ProjectCapability {
                 type_name: "wifi".to_string(),
                 name: "WiFi Connection".to_string(),
                 hint: "Enables WiFi connectivity.".to_string()
             }, ProjectCapability {
+                type_name: "ethernet".to_string(),
+                name: "Ethernet Connection".to_string(),
+                hint: "Enables Ethernet connectivity.".to_string()
+            }, ProjectCapability {
                 type_name: "bluetooth".to_string(),
                 name: "Bluetooth Connection".to_string(),
                 hint: "Enables Bluetooth connectivity.".to_string()
+            }, ProjectCapability {
+                type_name: "cellular".to_string(),
+                name: "Cellular connection".to_string(),
+                hint: "Enables Cellular connectivity.".to_string()
+            }, ProjectCapability {
+                type_name: "nfc".to_string(),
+                name: "NFC connection".to_string(),
+                hint: "Enables Near field communication connectivity.".to_string()
+            }, ProjectCapability {
+                type_name: "lora".to_string(),
+                name: "LoRa WAN connection".to_string(),
+                hint: "Enables LoRa WAN connectivity.".to_string()
             }]
         }, ProjectCapabilityGroup {
             type_name: "output".to_string(),
@@ -149,9 +164,21 @@ impl DB {
                 name: "LED Indicator".to_string(),
                 hint: "Allows your device to light up LEDs whenever needed.".to_string()
             }, ProjectCapability {
+                type_name: "led-display".to_string(),
+                name: "LED Display".to_string(),
+                hint: "Allows your device to use basic LED display.".to_string()
+            }, ProjectCapability {
+                type_name: "lcd-display".to_string(),
+                name: "Display".to_string(),
+                hint: "Allows your device to use LCD.".to_string()
+            }, ProjectCapability {
                 type_name: "speaker".to_string(),
                 name: "Speaker".to_string(),
                 hint: "Allows your device to play sounds and music.".to_string()
+            },ProjectCapability {
+                type_name: "beeper".to_string(),
+                name: "Beeper".to_string(),
+                hint: "Allows your device to produce very basic sounds or beeps.".to_string()
             }]
         }, ProjectCapabilityGroup {
             type_name: "input".to_string(),
@@ -166,6 +193,62 @@ impl DB {
                 type_name: "light".to_string(),
                 name: "Light Sensor".to_string(),
                 hint: "Allows your device to react on changes in light conditions.".to_string()
+            }, ProjectCapability {
+                type_name: "humidity".to_string(),
+                name: "Humidity Sensor".to_string(),
+                hint: "Allows your device to measure humidity level.".to_string()
+            }, ProjectCapability {
+                type_name: "motion".to_string(),
+                name: "Motion Sensor".to_string(),
+                hint: "Allows your device to detect motion.".to_string()
+            }, ProjectCapability {
+                type_name: "temperature".to_string(),
+                name: "Temperature Sensor".to_string(),
+                hint: "Allows your device to measure temperature.".to_string()
+            }, ProjectCapability {
+                type_name: "magnetic".to_string(),
+                name: "Magnetic Field Sensor".to_string(),
+                hint: "Allows your device to detect changes in magnetic field.".to_string()
+            }, ProjectCapability {
+                type_name: "air-pollution".to_string(),
+                name: "Air Pollution Sensor".to_string(),
+                hint: "Allows your device to measure air pollution level.".to_string()
+            }]
+        }, ProjectCapabilityGroup {
+            type_name: "mechanical".to_string(),
+            name: "Mechanical".to_string(),
+            hint: "Items in this group allows your device to perform mechanical \
+                actions.".to_string(),
+            capabilities: vec![ProjectCapability {
+                type_name: "motor".to_string(),
+                name: "DC Motor".to_string(),
+                hint: "Allows your device to spin a DC motor.".to_string()
+            }, ProjectCapability {
+                type_name: "servo".to_string(),
+                name: "Servo Motor".to_string(),
+                hint: "Allows your device to control a servo motor.".to_string()
+            }, ProjectCapability {
+                type_name: "vibrator".to_string(),
+                name: "Vibrator".to_string(),
+                hint: "Allows your device to vibrate.".to_string()
+            }]
+        }, ProjectCapabilityGroup {
+            type_name: "switches".to_string(),
+            name: "Switches".to_string(),
+            hint: "Items in this group allows your device to have various switches.".to_string(),
+            capabilities: vec![ProjectCapability {
+                type_name: "momentary-switch".to_string(),
+                name: "Momentary Switch".to_string(),
+                hint: "Allows your device to have a mechanical momentary switch.".to_string()
+            }, ProjectCapability {
+                type_name: "maintained-switch".to_string(),
+                name: "Maintained Switch".to_string(),
+                hint: "Allows your device to have a mechanical switch with a maintained \
+                    state.".to_string()
+            }, ProjectCapability {
+                type_name: "slide-switch".to_string(),
+                name: "Slide Switch".to_string(),
+                hint: "Allows your device to have a slide switch.".to_string()
             }]
         }];
 
