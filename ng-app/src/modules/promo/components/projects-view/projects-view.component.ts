f2afc34f969e2651c42c7ddeaadc0d73aa1372d5
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2017-06-29 23:32:46 +0200

    Add rudimentary project management UI.

--- ng-app/src/modules/promo/components/projects-view/projects-view.component.ts
+++ ng-app/src/modules/promo/components/projects-view/projects-view.component.ts
@@ -1,8 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {ProjectService} from '../../../app/services/project.service';
+import {Project} from '../../../app/core/projects/project';
 
 @Component({
   templateUrl: 'projects-view.component.html',
   styleUrls: ['projects-view.component.css']
 })
-export class ProjectsViewComponent {
+export class ProjectsViewComponent implements OnInit {
+  projects: Project[] = [];
+
+  constructor(private projectService: ProjectService) {
+  }
+
+  ngOnInit() {
+    this.projectService.getProjects().subscribe((projects) => this.projects = projects);
+  }
+
+  deleteProject(project: Project, index: number) {
+    this.projectService.deleteProject(project.id).subscribe(() => {
+      this.projects.splice(index, 1);
+    });
+  }
 }
