b3a4fbb719333d4e37bdcf36eec614f0de874d7e
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2017-07-04 23:16:22 +0200

    Introduce basic property editor component.

---
+++ ng-app/src/modules/app/components/property-editor/property-editor.component.ts
@@ -0,0 +1,30 @@
+import {Component, Input, OnChanges, OnDestroy} from '@angular/core';
+import {FormControl} from '@angular/forms'
+
+import {ComponentPropertySchema} from '../../core/components/component-property-schema';
+
+interface IProperty {
+  schema: ComponentPropertySchema;
+  storage: Map<string, string>;
+}
+
+@Component({
+  selector: 'frunze-property-editor',
+  templateUrl: 'property-editor.component.html',
+  styleUrls: ['property-editor.component.css']
+})
+export class PropertyEditorComponent implements OnChanges, OnDestroy {
+  @Input() property: IProperty;
+  valueEditor = new FormControl();
+
+  ngOnChanges() {
+    const value = !this.property ?
+      '' :
+      this.property.storage.get(this.property.schema.type) || this.property.schema.default;
+    this.valueEditor.setValue(value)
+  }
+
+  ngOnDestroy() {
+    this.property.storage.set(this.property.schema.type, this.valueEditor.value);
+  }
+}
