76ca42afabbe262d0755fd5d4a1357c79d8da9cf
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2017-08-02 21:07:48 +0200

    Improve Bom calculation (respect moq, item count etc.).

--- ng-app/src/modules/app/components/bom-calculation-view/bom-calculation-view.component.ts
+++ ng-app/src/modules/app/components/bom-calculation-view/bom-calculation-view.component.ts
@@ -10,7 +10,6 @@ import {ComponentsService} from '../../services/components.service';
 import {ProjectService} from '../../services/project.service';
 import {BomService} from '../../services/bom.service';
 import {Project} from '../../core/projects/project';
-import {ProjectComponent} from '../../core/projects/project-component';
 import {ComponentSchema} from '../../core/components/component-schema';
 import {Part} from '../../core/bom/part';
 
@@ -20,13 +19,19 @@ import {Part} from '../../core/bom/part';
 })
 export class BomCalculationViewComponent implements OnInit {
   project: Project;
-  componentSchemas: Map<string, ComponentSchema>;
   bom: {
-    mpn: string,
-    part: Part,
-    component: ProjectComponent,
-    schema: ComponentSchema
-  }[] = [];
+    items: {
+      mpn: string,
+      count: number,
+      url: string,
+      price: number,
+      schema: ComponentSchema
+    }[],
+    totalPrice: number
+  } = {
+    items: [],
+    totalPrice: 0
+  };
 
   constructor(private route: ActivatedRoute, private router: Router,
               private bomService: BomService,
@@ -44,30 +49,12 @@ export class BomCalculationViewComponent implements OnInit {
         })
         .switchMap(([schemas, project]) => {
           this.project = project;
-          this.componentSchemas = schemas;
-
-          const mpns = [];
-          for (const component of this.project.components) {
-            const mpn = this.componentSchemas.get(component.type).mpn;
-            if (!mpn) {
-              continue;
-            }
-            mpns.push(mpn);
-          }
-
-          return this.bomService.getForMpns(mpns);
+          return this.bomService.getForMpns(this.updateProjectMPNs(schemas, project));
         })
-        .subscribe((bom: Map<string, Part>) => {
-          this.bom = Array.from(bom).map(([mpn, part]) => {
-            const component = this.project.components.find((projectComponent) => {
-              return this.componentSchemas.get(projectComponent.type).mpn === mpn;
-            });
-
-            return {mpn, part, component, schema: this.componentSchemas.get(component.type)};
-          });
-        }, (e) => {
-            console.error('Error occurred while retrieving of project.', e);
-        });
+        .subscribe(
+          (bom: Map<string, Part>) => this.updatePrices(bom),
+          (e) => console.error('Error occurred while retrieving of project.', e)
+        );
   }
 
   onNext() {
@@ -78,5 +65,50 @@ export class BomCalculationViewComponent implements OnInit {
 
   onGoToProjectSoftware() {
     this.router.navigate([`promo/project/software/${this.project.id}`]);
+  }
+
+  private updateProjectMPNs(schemas: Map<string, ComponentSchema>, project: Project) {
+    this.bom.items = [];
+    const mpns = new Set<string>();
+    for (const component of this.project.components) {
+      const schema = schemas.get(component.type);
+      if (!schema.mpn) {
+        continue;
+      }
+
+      if (mpns.has(schema.mpn)) {
+        this.bom.items.find((item) => item.mpn === schema.mpn).count++;
+      } else {
+        this.bom.items.push({
+          mpn: schema.mpn,
+          count: 1,
+          url: '',
+          price: 0,
+          schema: schema
+        });
+
+        mpns.add(schema.mpn);
+      }
+    }
+
+    return Array.from(mpns)
+  }
+
+  private updatePrices(bom: Map<string, Part>) {
+    for (const item of this.bom.items) {
+      if (!bom.has(item.mpn)) {
+        continue;
+      }
+
+      const part = bom.get(item.mpn);
+      item.url = part.url;
+
+      // Find offer with minimum order quantity of one item and price for this "batch".
+      const bestOffer = part.offers.find((offer) => offer.moq === 1 && offer.prices.has(1));
+      if (bestOffer) {
+        item.price = bestOffer.prices.get(1);
+        this.bom.totalPrice += item.price * item.count;
+      }
+    }
   }
 }
